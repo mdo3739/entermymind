dd665cfcf981db28ca55e73dc632ed7bd1f31c17
Michael Do <mdo3739@gmail.com>
2015-10-06 22:44:02 -0500

    finished friendship model

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,28 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Added to your list"
+    @friend = User.find(params[:friend_id])
+    if current_user.friends.include?(@friend)
+      flash[:alert] = "#{@friend.username} is already on your list!"
       redirect_to :back
     else
-      flash[:error] = "Didn't work. Try again?"
+      @friendship = current_user.friendships.build(friend_id: @friend.id)
+      if @friendship.save
+        flash[:notice] = "Added to your list"
+        redirect_to :back
+      else
+        flash[:error] = "Didn't work. Try again?"
+        redirect_to :back
+      end
+    end
+  end
+
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    if @friendship.destroy
+      flash[:notice] = "Removed from your circle"
+      redirect_to :back
+    else
+      flash[:error] = "Did not work. Try again."
       redirect_to :back
     end
   end
